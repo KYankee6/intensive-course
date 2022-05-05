1f913506484b892158437daba13e2f9956bbbc96
KYankee6 <highestbright98@naver.com>
2022-05-05 22:58:14 +0900

    task json git ignoring

---
+++ Week1/python-for-coding-test/4/4-3.cpp
@@ -0,0 +1,26 @@
+// 2022-05-05 22:49:38
+
+#include <bits/stdc++.h>
+#define N 8
+#define in_range(x,y) x>=0 && y>=0 && x<N && y<N
+using namespace std;
+
+int main(){
+    int dx[8]={2,2,-2,-2,1,1,-1,-1};
+    int dy[8]={1,-1,1,-1,2,-2,2,-2};
+    vector<vector<int>> board(N,vector<int>(N,0));
+    int answer=0;
+    int x,y;
+    string s;
+    cin>>s;
+    x=s[0]-'a';
+    y=s[1]-'1';
+
+    for(int i=0; i<8; i++){
+        int px = x+dx[i];
+        int py = y+dy[i];
+        if(in_range(px,py))answer++;
+    }
+    cout<<answer;
+
+}
