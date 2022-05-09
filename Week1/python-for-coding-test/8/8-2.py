35be39d997b0e907eb803eebd9247123444e98e6
KYankee6 <highestbright98@naver.com>
2022-05-10 00:02:06 +0900

    8-2.py

---
+++ Week1/python-for-coding-test/8/8-2.py
@@ -0,0 +1,13 @@
+ # 피보나치 수열 (메모이제이션)
+d = [0] * 100
+
+
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x]=fibo(x-1)+fibo(x-2) # 아직 계산하지 않은 문제
+    return d[x]
+
+print(fibo(99))
