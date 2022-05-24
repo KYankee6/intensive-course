1efbbe7066b91566fd1f583d5562b5b393ee8092
Kyankee6 <didzl1231@naver.com>
2022-05-24 19:43:36 +0900

    구조변경

---
+++ Week1/python-for-coding-test/15-binary_search/30.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define n_child 26
+using namespace std;
+
+int to_idx(char a)
+{
+    return a - 'a';
+}
+
+struct Trie
+{
+    char x;
+    Trie *childs[n_child];
+    bool isEnd;
+    Trie(){};
+    Trie(int _x) : x(_x)
+    {
+        fill(childs, childs + n_child, nullptr);
+        isEnd = false;
+    };
+    ~Trie()
+    {
+        for (int i = 0; i < n_child; i++)
+        {
+            if (childs[i])
+            {
+                delete childs[i];
+            }
+        }
+    }
+    void insert(int idx, string &str)
+    {
+        if (idx == str.size() - 1)
+        {
+            isEnd = true;
+        }
+        else
+        {
+            if (childs[to_idx(str[idx])] == nullptr)
+            {
+                Trie *new_trie = new Trie(str[idx]);
+                childs[to_idx(str[idx])] = new_trie;
+            }
+            insert(idx + 1, str);
+        }
+    }
+    bool search(int idx, string &target)
+    {
+        if (idx == target.size() && childs[to_idx(target[idx - 1])]->isEnd)
+        {
+            return true;
+        }
+        else if (target[idx] == '?' || childs[to_idx(target[idx])])
+        {
+            return search(idx + 1, target);
+        }
+        else
+            return false;
+    }
+};
+
+vector<int> solution(vector<string> words, vector<string> queries)
+{
+    vector<int> answer;
+    Trie *root = new Trie('.');
+    for (int i = 0; i < words.size(); i++)
+    {
+        root->insert(0, words[i]);
+    }
+    
+    return answer;
+}
