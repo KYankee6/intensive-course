c931dca374783b161d05f79831175acb30d40a9a
Kyankee6 <didzl1231@naver.com>
2022-06-06 17:52:06 +0900

    q37

--- Week1/python-for-coding-test/17 - graph/q37.cpp
+++ Week1/python-for-coding-test/17 - graph/q37.cpp
@@ -1,18 +1,8 @@
 #include <bits/stdc++.h>
+#define INF 100000001
 using namespace std;
 
-struct node
-{
-    int x;
-    int y;
-    int cost;
-
-    node(){};
-    node(int _x, int _y, int _cost) : x(_x), y(_y), cost(_cost){};
-};
-
-vector<node> graph;
-vector<vector<int>> dist;
+vector<vector<int>> graph;
 int n, m;
 int main()
 {
@@ -21,14 +11,41 @@ int main()
     cout.tie(nullptr);
 
     cin >> n >> m;
-    graph.resize(n);
-    dist.resize(n, vector<int>(n, INT_MAX));
+    graph.resize(n, vector<int>(n, INF));
 
     for (int i = 0; i < m; i++)
     {
         int src, dest, cost;
         cin >> src >> dest >> cost;
-        node newNode(src - 1, dest - 1, cost);
-        graph.push_back(newNode);
+        src--;
+        dest--;
+        graph[src][dest] = min(graph[src][dest],cost);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        graph[i][i] = 0;
+    }
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int a = 0; a < n; a++)
+        {
+            for (int b = 0; b < n; b++)
+            {
+                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < n; j++)
+        {
+            if (graph[i][j]==INF)
+                cout << 0 << " ";
+            else
+                cout << graph[i][j] << " ";
+        }
+        cout << '\n';
     }
 }
