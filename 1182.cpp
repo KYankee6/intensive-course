0f58b06ca52b43853fff15f54ba70a9fcab14438
KYankee6 <highestbright98@naver.com>
2023-10-21 12:38:13 +0900

    acmicpc.net 1182.cpp

---
+++ 1182.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int answer = 0;
+    int N;
+    int K;
+    vector<int> v;
+    cin >> N >> K;
+    v.resize(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> v[i];
+    }
+
+    // your code here;
+    for(int i=0; i<N; i++){
+
+    }
+
+
+    cout << answer << endl;
+    return 0;
+}
